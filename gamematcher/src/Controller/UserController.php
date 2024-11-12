ab6e1b9fd6989d182248c2ebfa7d5eba6f16a202
daniel <daniel.mor.muj@gmail.com>
2024-11-12 14:20:16 +0000

    Landing page in progress

--- gamematcher/src/Controller/UserController.php
+++ gamematcher/src/Controller/UserController.php
@@ -10,8 +10,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 #[Route('/user')]
+
 class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
